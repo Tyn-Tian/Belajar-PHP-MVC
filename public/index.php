7673d89e80e2a0f3960e5c103c461931cfc86035
Christian <kristian2015ok@gmail.com>
2024-04-30 11:25:06 +0700

    Add: Router

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-$path = "/index";
+require_once __DIR__ . "/../vendor/autoload.php";
 
-if (isset($_SERVER["PATH_INFO"])) {
-    $path = $_SERVER["PATH_INFO"];
-}
+use TynChristian\Belajar\PHP\MVC\App\Router;
 
-require __DIR__ . "/../app/View" . $path . ".php";
+Router::add("GET", "/", "HomeController", "index");
+Router::add("GET", "/hello", "HelloController", "hello");
+Router::add("GET", "/world", "WorldController", "world");
+
+Router::run();
